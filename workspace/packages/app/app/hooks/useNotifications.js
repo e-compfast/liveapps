668b1b9c235102cf9ff0a788fc4c1c35a9544871
E-compfast Digital Indonesia <alamhafidz01@gmail.com>
2022-01-11 12:56:39 +0000

    with lerna init

---
+++ workspace/packages/app/app/hooks/useNotifications.js
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
+
+import expoPushTokensApi from '../api/expoPushTokens';
+import logger from '../utility/logger';
+
+export default useNotifications = (notificationListener) => {
+  useEffect(() => {
+    registerForPushNotifications();
+
+    if (notificationListener) {
+      Notifications.addListener(notificationListener);
+    }
+  }, []);
+
+  const registerForPushNotifications = async () => {
+    try {
+      const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+
+      if (!permission.granted) {
+        return;
+      }
+
+      const token = await Notifications.getExpoPushTokenAsync();
+      expoPushTokensApi.register(token);
+    } catch (error) {
+      logger.log(error);
+    }
+  };
+};
---
+++ workspace/packages/app/app/screens/ListingsScreen.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect } from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+
+import ActivityIndicator from '../components/ActivityIndicator';
+import AppButton from '../components/AppButton';
+import Card from '../components/Card';
+import colors from '../config/colors';
+import listingsApi from '../api/listings';
+import routes from '../navigation/routes';
+import Screen from '../components/Screen';
+import AppText from '../components/AppText';
+import useApi from '../hooks/useApi';
+
+function ListingsScreen({ navigation }) {
+  const { data: listings, error, loading, request: loadListings } = useApi(
+    listingsApi.getListings
+  );
+
+  useEffect(() => {
+    loadListings();
+  }, []);
+
+  return (
+    <>
+      <ActivityIndicator visible={loading} />
+      <Screen style={styles.screen}>
+        {error && (
+          <>
+            <AppText>Couldn't retrieve the listings.</AppText>
+            <AppButton text='Retry' onPress={loadListings} />
+          </>
+        )}
+
+        <FlatList
+          data={listings}
+          keyExtractor={(listing) => listing.id.toString()}
+          renderItem={({ item }) => (
+            <Card
+              title={item.title}
+              subTitle={`$${item.price}`}
+              imageUrl={item.images[0].url}
+              thumbnailUrl={item.images[0].thumbnailUrl}
+              onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
+            />
+          )}
+        />
+      </Screen>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 20,
+    backgroundColor: colors.light,
+  },
+});
+
+export default ListingsScreen;
